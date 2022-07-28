c6e3a4ba7747b36d6a08932d51615f1fab11f521
Ketchua_ <enzobarret.zozo@gmail.com>
2022-07-29 00:01:48 +0200

    Effet Radiations + AntimatièreIngot

---
+++ src/main/java/fr/osalys/mod/procedures/AntimatiereIngotQuandUneEntiteVivanteEstFrappeeAvecLitemProcedure.java
@@ -0,0 +1,24 @@
+package fr.osalys.mod.procedures;
+
+import net.minecraft.potion.Effects;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+
+import java.util.Map;
+
+import fr.osalys.mod.OsalysmodMod;
+
+public class AntimatiereIngotQuandUneEntiteVivanteEstFrappeeAvecLitemProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				OsalysmodMod.LOGGER.warn("Failed to load dependency entity for procedure AntimatiereIngotQuandUneEntiteVivanteEstFrappeeAvecLitem!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.POISON, (int) 300, (int) 3, (true), (true)));
+	}
+}
---
+++ src/main/java/fr/osalys/mod/procedures/RadiationsTickActifSurEffetProcedure.java
@@ -0,0 +1,61 @@
+package fr.osalys.mod.procedures;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.world.IWorld;
+import net.minecraft.potion.Effects;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+
+import java.util.Map;
+
+import fr.osalys.mod.OsalysmodMod;
+
+public class RadiationsTickActifSurEffetProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				OsalysmodMod.LOGGER.warn("Failed to load dependency world for procedure RadiationsTickActifSurEffet!");
+			return;
+		}
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				OsalysmodMod.LOGGER.warn("Failed to load dependency entity for procedure RadiationsTickActifSurEffet!");
+			return;
+		}
+		IWorld world = (IWorld) dependencies.get("world");
+		Entity entity = (Entity) dependencies.get("entity");
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.POISON, (int) 600, (int) 1, (false), (false)));
+		new Object() {
+			private int ticks = 0;
+			private float waitTicks;
+			private IWorld world;
+
+			public void start(IWorld world, int waitTicks) {
+				this.waitTicks = waitTicks;
+				MinecraftForge.EVENT_BUS.register(this);
+				this.world = world;
+			}
+
+			@SubscribeEvent
+			public void tick(TickEvent.ServerTickEvent event) {
+				if (event.phase == TickEvent.Phase.END) {
+					this.ticks += 1;
+					if (this.ticks >= this.waitTicks)
+						run();
+				}
+			}
+
+			private void run() {
+				if (entity instanceof LivingEntity)
+					((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.NAUSEA, (int) 600, (int) 1, (false), (false)));
+				MinecraftForge.EVENT_BUS.unregister(this);
+			}
+		}.start(world, (int) 200);
+	}
+}
