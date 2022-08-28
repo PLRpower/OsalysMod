87d226dead14829e6228270311b4a79ec68077d2
Stryyke18 <YANNIS@DESKTOP-P4ITJJ1>
2022-08-28 18:32:51 +0200

    Ajout de OsaliumApple et de SaphirApple avec chaqu'un leurs procédure

---
+++ src/main/java/fr/osalys/mod/item/OsaliumAppleItem.java
@@ -0,0 +1,66 @@
+
+package fr.osalys.mod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.block.BlockState;
+
+import java.util.stream.Stream;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.AbstractMap;
+
+import fr.osalys.mod.procedures.OsaliumAppleLeJoueurTermineDutiliserLitemProcedure;
+import fr.osalys.mod.itemgroup.OsalysTabItemGroup;
+import fr.osalys.mod.OsalysmodModElements;
+
+@OsalysmodModElements.ModElement.Tag
+public class OsaliumAppleItem extends OsalysmodModElements.ModElement {
+	@ObjectHolder("osalysmod:osalium_apple")
+	public static final Item block = null;
+
+	public OsaliumAppleItem(OsalysmodModElements instance) {
+		super(instance, 246);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(OsalysTabItemGroup.tab).maxStackSize(64).rarity(Rarity.EPIC)
+					.food((new Food.Builder()).hunger(4).saturation(0.6f).setAlwaysEdible().build()));
+			setRegistryName("osalium_apple");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		public ItemStack onItemUseFinish(ItemStack itemstack, World world, LivingEntity entity) {
+			ItemStack retval = super.onItemUseFinish(itemstack, world, entity);
+			double x = entity.getPosX();
+			double y = entity.getPosY();
+			double z = entity.getPosZ();
+
+			OsaliumAppleLeJoueurTermineDutiliserLitemProcedure.executeProcedure(Stream.of(new AbstractMap.SimpleEntry<>("entity", entity))
+					.collect(HashMap::new, (_m, _e) -> _m.put(_e.getKey(), _e.getValue()), Map::putAll));
+			return retval;
+		}
+	}
+}
