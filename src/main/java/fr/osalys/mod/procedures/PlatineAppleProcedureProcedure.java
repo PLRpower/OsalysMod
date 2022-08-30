104d2100ed1b78bf177c556b60aa08dce3ac9e47
YANNIS <YANNIS@DESKTOP-P4ITJJ1>
2022-08-30 15:47:05 +0200

    MCreator Merge commit 'af34753b7ff6d65c18ebe39ecd625da16afef9a7' into 02d00f38b166d079599325483c8e0bbe64a02a28

--- src/main/java/fr/osalys/mod/procedures/PlatineAppleProcedureProcedure.java
+++ src/main/java/fr/osalys/mod/procedures/PlatineAppleProcedureProcedure.java
@@ -1,13 +1,6 @@
 package fr.osalys.mod.procedures;
 
-import net.minecraft.potion.Effects;
-import net.minecraft.potion.EffectInstance;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.Entity;
-
-import java.util.Map;
-
-import fr.osalys.mod.OsalysmodMod;
+import net.minecraftforge.eventbus.api.Event;
 
 public class PlatineAppleProcedureProcedure {
 
@@ -17,7 +10,9 @@ public class PlatineAppleProcedureProcedure {
 				OsalysmodMod.LOGGER.warn("Failed to load dependency entity for procedure PlatineAppleProcedure!");
 			return;
 		}
+
 		Entity entity = (Entity) dependencies.get("entity");
+
 		if (entity instanceof LivingEntity)
 			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.SPEED, (int) 1200, (int) 0, (false), (false)));
 		if (entity instanceof LivingEntity)
@@ -29,4 +24,5 @@ public class PlatineAppleProcedureProcedure {
 		if (entity instanceof LivingEntity)
 			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.REGENERATION, (int) 300, (int) 2, (false), (false)));
 	}
+
 }
